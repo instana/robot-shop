979b5c839daa81ea69adeaf0577d8b0184d9e0e4
Steve Waterworth <s@steveww.org>
2018-02-06 16:00:17 -0500

    improved error checking

--- load-gen/run.sh
+++ load-gen/run.sh
@@ -6,5 +6,12 @@ then
     exit 1
 fi
 
+TEST=$(echo "$HOST" | egrep '^http://[a-z]+')
+if [ -z "$TEST" ]
+then
+    echo "Host must start with http://"
+    exit 1
+fi
+
 locust -f robot-shop.py --host "$HOST" --no-web -c 1 -r 1
 
