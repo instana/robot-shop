d868935737bac20fcacb6ade8c877f5dbab5c3c4
Steve Waterworth <s@steveww.org>
2018-02-06 16:08:29 -0500

    bug fix

--- load-gen/build.sh
+++ load-gen/build.sh
@@ -11,5 +11,6 @@ docker build -t ${REPO}/rs-load:${TAG} . && docker tag ${REPO}/rs-load:${TAG} ${
 if [ "$1" = "push" ]
 then
     echo "pushing..."
-    docker push ${REPO}/rs-load:${TAG} ${REPO}/rs-load
+    docker push ${REPO}/rs-load:${TAG}
+    docker push ${REPO}/rs-load
 fi
