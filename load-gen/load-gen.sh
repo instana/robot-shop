16b4a20d61601d0a858d9e542a2a026f01270a14
Steve Waterworth <s@steveww.org>
2018-03-21 08:40:42 +0000

    use .env file

--- load-gen/load-gen.sh
+++ load-gen/load-gen.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# get the tag info
+eval $(egrep '[A-Z]+=' ../.env)
+
+echo "Repo $REPO"
+echo "Tag $TAG"
 
 docker run \
     -it \
     --rm \
     --network=host \
     -e 'HOST=http://localhost:8080' \
-    steveww/rs-load
+    ${REPO}/rs-load:${TAG}
 
