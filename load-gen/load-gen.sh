7e4b55b19dee625b5a1f7b2ee9f86687e9b88461
Steve Waterworth <s@steveww.org>
2018-10-22 15:43:40 +0100

    improve script config

--- load-gen/load-gen.sh
+++ load-gen/load-gen.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# set -x
+
 # Changing the NUM_CLIENTS environment variable varies the load on the application
 # The bigger the number the more requests, the bigger the load
+NUM_CLIENTS=1
 
-# Set SILENT to anything to have all output discarded. Useful when running load for
-# a long time to stop the disk filling up with copious log messages
-# -e 'SILENT=1'
+# HOST where Stan's Robot Shop web UI is running
+HOST="http://localhost:8080"
+
+if [ ! -f ../.env ]
+then
+    echo "Please run this script from the load-gen directory"
+    exit 1
+fi
 
 # get the tag info
 eval $(egrep '[A-Z]+=' ../.env)
@@ -20,8 +28,8 @@ then
         -d \
         --rm \
         --network=host \
-        -e 'HOST=http://localhost:8080' \
-        -e 'NUM_CLIENTS=1' \
+        -e "HOST=$HOST" \
+        -e "NUM_CLIENTS=$NUM_CLIENTS" \
         -e 'SILENT=1' \
         ${REPO}/rs-load:${TAG}
 else
@@ -29,8 +37,8 @@ else
         -it \
         --rm \
         --network=host \
-        -e 'HOST=http://localhost:8080' \
-        -e 'NUM_CLIENTS=1' \
+        -e "HOST=$HOST" \
+        -e "NUM_CLIENTS=$NUM_CLIENTS" \
         ${REPO}/rs-load:${TAG}
 fi
 
