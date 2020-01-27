cbaab6a8988995717ef496ecdd0fbb5e805e25f0
Steve Waterworth <s@steveww.org>
2020-01-27 17:35:50 +0000

    use -z flag

--- OpenShift/setup.sh
+++ OpenShift/setup.sh
@@ -18,7 +18,7 @@ done
 
 oc adm new-project robot-shop --node-selector='type=infra'
 oc adm policy add-role-to-user admin developer -n robot-shop
-oc adm policy add-scc-to-user anyuid system:serviceaccount:robot-shop:default
+oc adm policy add-scc-to-user anyuid -z default
 
 oc logout
 
