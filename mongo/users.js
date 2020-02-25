5cf7bf8a2bab376c93003b54c373b88d56f1085f
Steve Waterworth <s@steveww.org>
2020-02-25 16:15:08 +0000

    add partner account

--- mongo/users.js
+++ mongo/users.js
@@ -4,7 +4,8 @@
 db = db.getSiblingDB('users');
 db.users.insertMany([
     {name: 'user', password: 'password', email: 'user@me.com'},
-    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'}
+    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'},
+    {name: 'partner-57', password: 'worktogether', email: 'howdy@partner.com'}
 ]);
 
 // unique index on the name
