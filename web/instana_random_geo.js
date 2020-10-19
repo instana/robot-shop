83ab82407ca7c2b4bd58a9d7cd6474d7fb718482
Cedric Ziel <cedric.ziel@instana.com>
2020-10-19 11:55:31 +0200

    Showcase geo tags for calls derived from x-forwarded-for header

---
+++ web/instana_random_geo.js
@@ -0,0 +1,21 @@
+var items = [
+    // white house
+    "156.33.241.5",
+    // Hollywood
+    "34.196.93.245",
+    // Chicago
+    "98.142.103.241",
+    // Los Angeles
+    "192.241.230.151"
+];
+
+// we get a random ip address to simulate specific locations of the requester
+function get(r) {
+    if (process.env.NGINX_INJECT_FAKE_IP != '1') {
+        return false;
+    }
+
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+export default get;
