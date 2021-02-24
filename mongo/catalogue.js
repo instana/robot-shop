db45fae8d64924c4d041a9a418c119a8df741c84
Dishant Kaushik <dkaushik95@gmail.com>
2021-02-24 14:21:57 -0500

    - removed licensed images - replaced with new images - changed the product image to png for consistency. - changed the names of robots and their SKU.

--- mongo/catalogue.js
+++ mongo/catalogue.js
@@ -3,17 +3,17 @@
 //
 db = db.getSiblingDB('catalogue');
 db.products.insertMany([
-    {sku: 'HAL-1', name: 'HAL', description: 'Sorry Dave, I cant do that', price: 2001, instock: 2, categories: ['Artificial Intelligence']},
-    {sku: 'PB-1', name: 'Positronic Brain', description: 'Highly advanced sentient processing unit with the laws of robotics burned in', price: 200, instock: 0, categories: ['Artificial Intelligence']},
-    {sku: 'ROB-1', name: 'Robbie', description: 'Large mechanical workhorse, crude but effective. Comes in handy when you are lost in space', price: 1200, instock: 12, categories: ['Robot']},
-    {sku: 'EVE-1', name: 'Eve', description: 'Extraterrestrial Vegetation Evaluator', price: 5000, instock: 10, categories: ['Robot']},
-    {sku: 'C3P0', name: 'C3P0', description: 'Protocol android', price: 953, instock: 1, categories: ['Robot']},
-    {sku: 'R2D2', name: 'R2D2', description: 'R2 maintenance robot and secret messenger. Help me Obi Wan', price: 1024, instock: 1, categories: ['Robot']},
-    {sku: 'K9', name: 'K9', description: 'Time travelling companion at heel', price: 300, instock: 12, categories: ['Robot']},
-    {sku: 'RD-10', name: 'Kryten', description: 'Red Drawf crew member', price: 700, instock: 5, categories: ['Robot']},
-    {sku: 'HHGTTG', name: 'Marvin', description: 'Marvin, your paranoid android. Brain the size of a planet', price: 42, instock: 48, categories: ['Robot']},
+    {sku: 'Alpha', name: 'Alpha', description: 'Sorry Dave, I cant do that', price: 2001, instock: 2, categories: ['Artificial Intelligence']},
+    {sku: 'Ewooid', name: 'Ewooid', description: 'Highly advanced sentient processing unit with the laws of robotics burned in', price: 200, instock: 0, categories: ['Artificial Intelligence']},
+    {sku: 'HPTD', name: 'High-Powered Travel Droid', description: 'Large mechanical workhorse, crude but effective. Comes in handy when you are lost in space', price: 1200, instock: 12, categories: ['Robot']},
+    {sku: 'UHJ', name: 'Ultimate Harvesting Juggernaut', description: 'Extraterrestrial Vegetation Evaluator', price: 5000, instock: 10, categories: ['Robot']},
+    {sku: 'EPE', name: 'Extreme Probe Emulator', description: 'Protocol android', price: 953, instock: 1, categories: ['Robot']},
+    {sku: 'EMM', name: 'Exceptional Medical Machine', description: 'R2 maintenance robot and secret messenger. Help me Obi Wan', price: 1024, instock: 1, categories: ['Robot']},
+    {sku: 'SHCE', name: 'Strategic Human Control Emulator', description: 'Time travelling companion at heel', price: 300, instock: 12, categories: ['Robot']},
+    {sku: 'RED', name: 'Responsive Enforcer Droid', description: 'Red Drawf crew member', price: 700, instock: 5, categories: ['Robot']},
+    {sku: 'RMC', name: 'Robotic Mining Cyborg', description: 'Marvin, your paranoid android. Brain the size of a planet', price: 42, instock: 48, categories: ['Robot']},
     {sku: 'STAN-1', name: 'Stan', description: 'APM guru', price: 67, instock: 1000, categories: ['Robot', 'Artificial Intelligence']},
-    {sku: 'STNG', name: 'Mr Data', description: 'Could be R. Daneel Olivaw? Protype positronic brain android', price: 1000, instock: 0, categories: ['Robot']}
+    {sku: 'CNA', name: 'Cybernated Neutralization Android', description: 'Could be R. Daneel Olivaw? Protype positronic brain android', price: 1000, instock: 0, categories: ['Robot']}
 ]);
 
 // full text index for searching
