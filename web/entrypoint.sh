d71c1216abe16c7981736d200a5c7f04ffde604a
SteveWW <s@steveww.org>
2021-06-25 11:30:16 +0100

    add Nginx tracing

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -29,5 +29,23 @@ chmod 644 $BASE_DIR/eum.html
 # apply environment variables to default.conf
 envsubst '${CATALOGUE_HOST} ${USER_HOST} ${CART_HOST} ${SHIPPING_HOST} ${PAYMENT_HOST} ${RATINGS_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
+# Patching for Instana tracing
+mv /tmp/ngx_http_opentracing_module.so /usr/lib/nginx/modules
+mv /tmp/libinstana_sensor.so /usr/local/lib
+cat - /etc/nginx/nginx.conf << !EOF! > /tmp/nginx.conf
+# Extra configuration for Instana tracing
+load_module modules/ngx_http_opentracing_module.so;
+
+# Pass through these env vars
+env INSTANA_SERVICE_NAME;
+env INSTANA_AGENT_HOST;
+env INSTANA_AGENT_PORT;
+env INSTANA_MAX_BUFFERED_SPANS;
+env INSTANA_DEV;
+!EOF!
+
+mv /tmp/nginx.conf /etc/nginx/nginx.conf
+echo "{}" > /etc/instana-config.json
+
 exec nginx-debug -g "daemon off;"
 
