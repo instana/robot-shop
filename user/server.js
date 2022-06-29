8b09ec7812691329411c6599872e98ea6c540e34
jradhakrishnan <radhakrishnan.jankiraman@gmail.com>
2022-06-29 09:58:41 +0530

    Add express instrumentation for NodeJS/Express services

--- user/server.js
+++ user/server.js
@@ -12,6 +12,7 @@ const mongoObjectID = require('mongodb').ObjectID;
 const redis = require('redis');
 const bodyParser = require('body-parser');
 const express = require('express');
+const promMid = require('express-prometheus-middleware');
 const pino = require('pino');
 const expPino = require('express-pino-logger');
 
@@ -34,6 +35,41 @@ const expLogger = expPino({
 const app = express();
 
 app.use(expLogger);
+
+
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    /**
+     * Uncomenting the `authenticate` callback will make the `metricsPath` route
+     * require authentication. This authentication callback can make a simple
+     * basic auth test, or even query a remote server to validate access.
+     * To access /metrics you could do:
+     * curl -X GET user:password@localhost:9091/metrics
+     */
+    // authenticate: req => req.headers.authorization === 'Basic dXNlcjpwYXNzd29yZA==',
+    /**
+     * Uncommenting the `extraMasks` config will use the list of regexes to
+     * reformat URL path names and replace the values found with a placeholder value
+    */
+    // extraMasks: [/..:..:..:..:..:../],
+    /**
+     * The prefix option will cause all metrics to have the given prefix.
+     * E.g.: `app_prefix_http_requests_total`
+     */
+    // prefix: 'app_prefix_',
+    /**
+     * Can add custom labels with customLabels and transformLabels options
+     */
+    // customLabels: ['contentType'],
+    // transformLabels(labels, req) {
+    //   // eslint-disable-next-line no-param-reassign
+    //   labels.contentType = req.headers['content-type'];
+    // },
+  }));
 
 app.use((req, res, next) => {
     res.set('Timing-Allow-Origin', '*');
