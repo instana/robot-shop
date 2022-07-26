289764b50a0ee6889f74716c69fcd07f95fc8750
Nandakumar Devi <nanda@asserts.ai>
2022-07-26 19:12:36 +0530

    Modify metric name since micrometer has a method_timed_seconds metric

--- dispatch/main.go
+++ dispatch/main.go
@@ -41,7 +41,7 @@ var (
 
 var methodDurationHistogram = prometheus.NewHistogramVec(
 	prometheus.HistogramOpts{
-		Name:    "method_timed_seconds",
+		Name:    "go_method_timed_seconds",
 		Help:    "histogram",
 		Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10},
 	},
