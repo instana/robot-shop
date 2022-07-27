e6395a337c8a041900fa0c673bafe8114dd82d6b
Nandakumar Devi <nanda@asserts.ai>
2022-07-27 17:15:18 +0530

    Setup mongodb, mysql & redis prometheus exporters as sidecars

--- K8s/install-all.sh
+++ K8s/install-all.sh
@@ -2,14 +2,6 @@ kubectl create ns robot-shop
 
 helm install robot-shop --namespace robot-shop helm
 
-helm install mysql-exporter prometheus-community/prometheus-mysql-exporter -f mysql/mysql-values.yaml -n robot-shop
-
-helm install redis-exporter prometheus-community/prometheus-redis-exporter -f redis/redis-values.yaml -n robot-shop
-
-helm install percona-mongodb prometheus-community/prometheus-mongodb-exporter -f mongodb/mongodb-exporter-values.yaml -n robot-shop
-
-kubectl apply -f mongodb/mongodb-exporter-servicemonitor.yaml
-
 # commented out since we already have an ingress controller running
 #helm install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx -f ingress/values.yaml -n robot-shop
 
