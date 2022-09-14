171dfbbed16c027e4dcb0412bd8d0ae09f9e2ccf
Nandakumar Devi <nanda@asserts.ai>
2022-09-14 18:36:27 +0530

    Versioning robot shop services

--- load-gen/build.sh
+++ load-gen/build.sh
@@ -6,11 +6,11 @@ eval $(egrep '[A-Z]+=' ../.env)
 echo "Repo $REPO"
 echo "Tag $TAG"
 
-docker build -t ${REPO}/robot-shop-rs-load:${TAG} . && docker tag ${REPO}/robot-shop-rs-load:${TAG} ${REPO}/robot-shop-rs-load
+docker build -t ${REPO}/robot-shop-rs-load:${LOAD_TAG} . && docker tag ${REPO}/robot-shop-rs-load:${LOAD_TAG} ${REPO}/robot-shop-rs-load
 
 if [ "$1" = "push" ]
 then
     echo "pushing..."
-    docker push ${REPO}/robot-shop-rs-load:${TAG}
+    docker push ${REPO}/robot-shop-rs-load:${LOAD_TAG}
     docker push ${REPO}/robot-shop-rs-load
 fi
