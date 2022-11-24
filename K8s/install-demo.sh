3cff2d3236334650f7d310c77712d63ab4347afa
Alex Ramos <aramos@asserts.ai>
2022-11-23 21:33:12 -0800

    templatize label and update install/diff

--- K8s/install-demo.sh
+++ K8s/install-demo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 ### robot-shop ingress for demo cluster located at: https://github.com/asserts/stage/blob/main/demo/resources/ingresses/nginx/ingress-demo.yaml in argo
+
 ### ingress-nginx controller for demo cluster located at: https://github.com/asserts/stage/blob/main/demo/apps/ingress-nginx.yaml in argo
 
-helm upgrade --install robot-shop --namespace robot-shop --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm
+# helm upgrade --install robot-shop --namespace robot-shop --set "serviceMonitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm
 
 # check diff
-# helm diff upgrade robot-shop --namespace robot-shop --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm --allow-unreleased
+helm diff upgrade robot-shop --namespace robot-shop --set "serviceMonitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm --allow-unreleased
 
