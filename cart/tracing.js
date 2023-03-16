d2e0b63a3adb911ad4f7f6156d150dac07e187b6
shanavazh <shanavazh@gmail.com>
2023-03-16 20:53:04 +0530

    using the otlp to generate traces.

--- cart/tracing.js
+++ cart/tracing.js
@@ -3,17 +3,18 @@
 const process = require('process');
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const { ExpressInstrumentation, ExpressRequestInfo } = require('@opentelemetry/instrumentation-express');
 
-const jaegerExporter = new JaegerExporter();
+const traceExporter = new OTLPTraceExporter()
+
 const sdk = new opentelemetry.NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'cart',
   }),
-  traceExporter: jaegerExporter,
+  traceExporter: traceExporter,
   instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()]
 });
 
