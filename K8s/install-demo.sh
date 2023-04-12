c2760d5f5892a3bbb8a938a5158d9239577ee69e
shanavazh <shanavazh@gmail.com>
2023-04-12 10:55:19 +0530

    deploy changes in demo

--- K8s/install-demo.sh
+++ K8s/install-demo.sh
@@ -4,8 +4,8 @@
 
 ### ingress-nginx controller for demo cluster located at: https://github.com/asserts/stage/blob/main/demo/apps/ingress-nginx.yaml in argo
 
-# helm upgrade --install robot-shop --namespace robot-shop --set "monitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm
+# helm upgrade --install robot-shop --namespace robot-shop --set "monitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" --set "otelcollector.assertsServer=https://demo.app.asserts.ai/api-server" --set "otelcollector.assertsUser=shana@asserts.ai" --set "otelcollector.assertsEnv=prod" --set "otelcollector.assertsSite=app" helm
 
 # check diff
-helm diff upgrade robot-shop --namespace robot-shop --set "monitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" helm --allow-unreleased
+helm diff upgrade robot-shop --namespace robot-shop --set "monitor.releaseLabel=kube-prometheus-stack" --set "redis.storageClassName=gp2" --set "rabbitmq.enableExporter=true" --set "otelcollector.assertsServer=https://demo.app.asserts.ai/api-server" --set "otelcollector.assertsUser=shana@asserts.ai" --set "otelcollector.assertsEnv=prod" --set "otelcollector.assertsSite=app" helm --allow-unreleased
 
